fca1c430b3fa48683a501a15ee0a7c8ae653649c
serjis17 <serjis17@gmail.com>
2020-09-04 13:06:14 +0530

    3 files modified

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -8,8 +8,18 @@ import { Component, OnInit } from '@angular/core';
 export class ProductsComponent implements OnInit {
 
   constructor() { }
+  products=[{name:'Moto G5',quantity:'2'},
+            {name:'Raycold Geyser',quantity:'3'},
+            {name:'Dell Inspiron Laptop',quantity:'1'}
+  ]
 
   ngOnInit() {
+  }
+  submit(f1,f2)
+  {
+    
+  
+   this.products.push({name:f1.value,quantity:f2.value})
   }
 
 }
